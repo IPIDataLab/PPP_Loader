81c0924ba5b196104476dfc1b5fbfeb9bdd6ed4e
IPINST <ipi@ipinst.org>
2015-04-10 10:42:21 -0400

    Update data_get.py

--- python/data_get.py
+++ python/data_get.py
@@ -19,7 +19,7 @@ def getNewData(date_str):
 	query = 'https://morph.io/IPINST/PPP_Scraper/data.json?key=P%2BC%2F7EFhx8IGx1ZW5N7D&query=select%20*%20from%20%22data%22%20where%20%22date%22%20%3D%20' + date_str
 
 	# Requests http request collating custom header and query date
-	r = requests.get(query)
+	r = requests.get(query, verify=False)
 
 	# Format data as json
 	update = r.json()
